8ea82ae6f1048a03550fed2491da8ea713628a10
Paul <paulmardling@hotmail.com>
2014-02-15 20:59:40 +0000

    Begun coding cleanup and refactoring

--- src/main/java/net/mardling/utils/AzureRestTemplate.java
+++ src/main/java/net/mardling/utils/AzureRestTemplate.java
@@ -2,6 +2,7 @@ package net.mardling.utils;
 
 import java.io.*;
 import java.net.URI;
+import java.net.URISyntaxException;
 
 import javax.net.ssl.*;
 import javax.xml.parsers.DocumentBuilder;
@@ -11,21 +12,13 @@ import net.mardling.azure.OperatingSystem;
 import net.mardling.azure.OperatingSystems;
 import net.mardling.azure.Subscription;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.HttpClients;
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Base64;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.xml.Jaxb2RootElementHttpMessageConverter;
 import org.apache.http.conn.ssl.AllowAllHostnameVerifier;
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.conn.ssl.X509HostnameVerifier;
@@ -34,9 +27,34 @@ import org.w3c.dom.*;
 
 import java.security.*;
 import java.util.Collections;
-import java.util.List;
 
+// TODO: Refactor cert store creation
 public class AzureRestTemplate {
+
+	private ClientHttpRequestFactory connFactory;
+
+	public AzureRestTemplate() {
+		try {
+
+			SSLSocketFactory factory = AzureRestTemplate
+					.getFactory(
+							AzureRestTemplate
+									.getBase64Cert("src/main/resources/AzureCredentials.xml"),
+							"output.txt");
+			X509HostnameVerifier verifier = new AllowAllHostnameVerifier();
+
+			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(
+					factory, verifier);
+
+			HttpClient httpClient = HttpClients.custom()
+					.setSSLSocketFactory(sslsf).build();
+			connFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+
+		} catch (Exception e) {
+
+		}
+
+	}
 
 	/*
 	 * Used to create the PKCS#12 store - important to note that the store is
@@ -46,8 +64,8 @@ public class AzureRestTemplate {
 	 * way otherwise BC can be used to load the cert into a keystore in advance
 	 * and password
 	 */
-	private static KeyStore createKeyStorePKCS12(String base64Certificate, String outstore)
-			throws Exception {
+	private static KeyStore createKeyStorePKCS12(String base64Certificate,
+			String outstore) throws Exception {
 		Security.addProvider(new BouncyCastleProvider());
 		KeyStore store = KeyStore.getInstance("PKCS12",
 				BouncyCastleProvider.PROVIDER_NAME);
@@ -66,63 +84,84 @@ public class AzureRestTemplate {
 		out.close();
 		return store;
 	}
+
+	private RestTemplate getAzureTemplate(ClientHttpRequestFactory factory) {
+		RestTemplate rest = new RestTemplate(connFactory);
+
+		CustomHeaderInterceptor addHeaders = new CustomHeaderInterceptor();
+		addHeaders.addHeader("x-ms-version", "2012-03-01");
+
+		rest.setInterceptors(Collections
+				.singletonList((ClientHttpRequestInterceptor) addHeaders));
+		
+		return rest;
+
+	}
+
 	
+	public Subscription getSubscription(String subsID) throws URISyntaxException {
+		URI uri = new URI("https://management.core.windows.net/" + subsID);
+
+
+		RestTemplate rest = getAzureTemplate(connFactory);
+
+		Subscription sub = (Subscription) rest.getForObject(uri,
+				Subscription.class);
+		
+		return sub;
+	}
+	
+	public OperatingSystems getOperatingSystems(String subsID) throws URISyntaxException {
+		URI uri = new URI("https://management.core.windows.net/" + subsID + "/operatingsystems");
+
+		RestTemplate rest = getAzureTemplate(connFactory);
+		
+		OperatingSystems os = (OperatingSystems) rest.getForObject(uri,OperatingSystems.class);
+		
+		return os;
+		
+	}
 	
 	public void restTemplate() {
 		try {
-		
-		SSLSocketFactory factory = AzureRestTemplate.getFactory(AzureRestTemplate.getBase64Cert("src/main/resources/AzureCredentials.xml"), "output.txt");
-		X509HostnameVerifier verifier = new AllowAllHostnameVerifier();
-		
-		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(factory, verifier);
+
+			URI uri = new URI(
+					"https://management.core.windows.net/72e280a7-f53d-4199-be45-3063afec8240");
 
 
-		HttpClient httpClient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
-		
+			RestTemplate rest = getAzureTemplate(connFactory);
 
-		
-		//URI uri = new URI("https://management.core.windows.net/72e280a7-f53d-4199-be45-3063afec8240/operatingsystems");
-		URI uri = new URI("https://management.core.windows.net/72e280a7-f53d-4199-be45-3063afec8240");
-		
-		ClientHttpRequestFactory connFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
-		RestTemplate rest = new RestTemplate(connFactory);
-		
-		CustomHeaderInterceptor addHeaders = new CustomHeaderInterceptor();
-		addHeaders.addHeader("x-ms-version", "2012-03-01");
-		
-		rest.setInterceptors(Collections.singletonList((ClientHttpRequestInterceptor)addHeaders));
-		
-		Subscription sub=(Subscription)rest.getForObject(uri,Subscription.class);
+			Subscription sub = (Subscription) rest.getForObject(uri,
+					Subscription.class);
 
-		
-		System.out.println("Subscription Name:" + sub.getSubscriptionName());
-		System.out.println("Subscription ID:" + sub.getSubscriptionID());
-		
-		
-		uri = new URI("https://management.core.windows.net/72e280a7-f53d-4199-be45-3063afec8240/operatingsystems");
-		
-		OperatingSystems os =(OperatingSystems)rest.getForObject(uri, OperatingSystems.class);
-		
-		System.out.println("OS " + os.getOperatingSystems().size());
-		
-		for (OperatingSystem myOs : os.getOperatingSystems()) {
-			System.out.println(myOs.getVersion());
-		}
-		
-		
-		} catch(Exception e) {
+			System.out
+					.println("Subscription Name:" + sub.getSubscriptionName());
+			System.out.println("Subscription ID:" + sub.getSubscriptionID());
+
+			uri = new URI(
+					"https://management.core.windows.net/72e280a7-f53d-4199-be45-3063afec8240/operatingsystems");
+
+			OperatingSystems os = (OperatingSystems) rest.getForObject(uri,
+					OperatingSystems.class);
+
+			System.out.println("OS " + os.getOperatingSystems().size());
+
+			for (OperatingSystem myOs : os.getOperatingSystems()) {
+				System.out.println(myOs.getVersion());
+			}
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
 
 	/*
 	 * Used to get an SSL factory from the keystore on the fly - this is then
 	 * used in the request to the service management which will match the
 	 * .publishsettings imported certificate
 	 */
-	private static SSLSocketFactory getFactory(String base64Certificate, String outStore)
-			throws Exception {
+	private static SSLSocketFactory getFactory(String base64Certificate,
+			String outStore) throws Exception {
 		KeyManagerFactory keyManagerFactory = KeyManagerFactory
 				.getInstance("SunX509");
 		KeyStore keyStore = createKeyStorePKCS12(base64Certificate, outStore);
@@ -135,33 +174,30 @@ public class AzureRestTemplate {
 		return context.getSocketFactory();
 	}
 
-
-
-
 	private static String getBase64Cert(String publishSettingsFile) {
-		String certificate=null;
+		String certificate = null;
 		try {
-		//this.publishSettingsFile="src/main/resources/AzureCredentials.xml";
+			// this.publishSettingsFile="src/main/resources/AzureCredentials.xml";
 
-		// Step 1: Read in the .publishsettings file
+			// Step 1: Read in the .publishsettings file
 
-		File file = new File(publishSettingsFile);
-		DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = dbf.newDocumentBuilder();
-		Document doc = db.parse(file);
-		doc.getDocumentElement().normalize();
-		// Step 2: Get the PublishProfile
-		NodeList ndPublishProfile = doc
-				.getElementsByTagName("PublishProfile");
-		Element publishProfileElement = (Element) ndPublishProfile.item(0);
-		// Step 3: Get the PublishProfile
-		certificate = publishProfileElement
-				.getAttribute("ManagementCertificate");
-		
+			File file = new File(publishSettingsFile);
+			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbf.newDocumentBuilder();
+			Document doc = db.parse(file);
+			doc.getDocumentElement().normalize();
+			// Step 2: Get the PublishProfile
+			NodeList ndPublishProfile = doc
+					.getElementsByTagName("PublishProfile");
+			Element publishProfileElement = (Element) ndPublishProfile.item(0);
+			// Step 3: Get the PublishProfile
+			certificate = publishProfileElement
+					.getAttribute("ManagementCertificate");
+
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return certificate;
 	}
 
--- src/test/java/net/mardling/utils/AzureRestTemplateTest.java
+++ src/test/java/net/mardling/utils/AzureRestTemplateTest.java
@@ -2,6 +2,12 @@ package net.mardling.utils;
 
 import static org.junit.Assert.*;
 
+import java.net.URISyntaxException;
+
+import net.mardling.azure.OperatingSystem;
+import net.mardling.azure.OperatingSystems;
+import net.mardling.azure.Subscription;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -11,10 +17,36 @@ public class AzureRestTemplateTest extends AzureRestTemplate {
 	public void setUp() throws Exception {
 	}
 
+	
 	@Test
-	public void testRestTemplate() {
+	public void testGetSubscription() {
 		AzureRestTemplate template=new AzureRestTemplate();
-		template.restTemplate();
+		
+		try {
+			Subscription subs = template.getSubscription("72e280a7-f53d-4199-be45-3063afec8240");
+			System.out.println(subs.getSubscriptionName());
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testGetOperatingSystems() {
+		AzureRestTemplate template=new AzureRestTemplate();
+		
+		try {
+			OperatingSystems os = template.getOperatingSystems("72e280a7-f53d-4199-be45-3063afec8240");
+			System.out.println("OS " + os.getOperatingSystems().size());
+
+			for (OperatingSystem myOs : os.getOperatingSystems()) {
+				System.out.println(myOs.getVersion());
+			}
+
+		} catch (URISyntaxException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
